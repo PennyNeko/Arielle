15c0f3d17b9ce3b55d4714855128f791769879b2
PennyNeko <penyamy@gmail.com>
2017-10-26 02:02:44 +0300

    added info module and test module

---
+++ Arielle/Arielle/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+
+namespace Arielle
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        => new Program().StartAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+
+        private CommandHandler _handler;
+
+        public async Task StartAsync()
+        {
+            _client = new DiscordSocketClient();
+            _handler = new CommandHandler(_client);
+            
+            string token = "MzcyODEzNjQwNjk4NjkxNTg0.DNJpYA.G1Gt0s12ekKpSwvSbqnWrgnOysA";
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
