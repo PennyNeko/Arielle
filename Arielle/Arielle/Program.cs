fde954b6daf6b48e90f1b0048b11430613c70838
PennyNeko <penyamy@gmail.com>
2017-10-27 13:07:23 +0300

    token passed through configuration file now

--- Arielle/Arielle/Program.cs
+++ Arielle/Arielle/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
 using Arielle.SaveLoad;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
+using System.Configuration;
 
 namespace Arielle
 {
@@ -31,26 +30,33 @@ namespace Arielle
                 settings.Converters.Add(new StringEnumConverter { CamelCaseText = false });
                 return settings;
             });
-
+            
             _client = new DiscordSocketClient();
             _handler = new CommandHandler(_client);
             
-            string token = "MzcyODEzNjQwNjk4NjkxNTg0.DNJpYA.G1Gt0s12ekKpSwvSbqnWrgnOysA";
-            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.LoginAsync(TokenType.Bot, ConfigurationManager.AppSettings["token"]);
             await _client.StartAsync();
 
             //Load the game users and place in list "users"
-            //LoadUsers loadedUsers = new LoadUsers();
-            //users = loadedUsers.GetUsers();
+            LoadUsers loadedUsers = new LoadUsers();
+            users = loadedUsers.GetUsers();
 
+            //Load game questions
             LoadQuestions loadedQuestions = new LoadQuestions();
             questions = loadedQuestions.GetQuestions();
 
             //Save the game users from list "users" to JSON file Users.json
-            //SaveUsers savedUsers = new SaveUsers(users);
+            SaveUsers savedUsers = new SaveUsers(users);
 
+            //Save game questions to JSON file
             SaveQuestions savedQuestions = new SaveQuestions(questions);
+
+            
             await Task.Delay(-1);
+        }
+        static void CurrentDomain_ProcessExit(object sender, EventArgs e)
+        {
+            Console.WriteLine("exit");
         }
     }
 }
