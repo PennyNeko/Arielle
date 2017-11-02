f09b4b97613f598821d1b0068383f0677db8137a
PennyNeko <penyamy@gmail.com>
2017-11-02 14:11:55 +0200

    listing the questions now sends the list as a private message

--- Arielle/Arielle/Modules/ListQuestions.cs
+++ Arielle/Arielle/Modules/ListQuestions.cs
@@ -1,8 +1,5 @@
-﻿using Discord.Commands;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Discord;
+using Discord.Commands;
 using System.Threading.Tasks;
 
 namespace Arielle.Modules
@@ -12,10 +9,12 @@ namespace Arielle.Modules
         [Command("ShowQuestions")]
         public async Task ShowQuestions()
         {
-            await Context.Channel.SendMessageAsync("List of all questions:");
+            var channel = Context.Channel;
+            IMessageChannel ch = await ((IGuildUser)Context.User).GetOrCreateDMChannelAsync();
+            await ch.SendMessageAsync("List of all questions:");
             foreach (var q in Program.Questions)
             {
-                await Context.Channel.SendMessageAsync($"{q.ID}: Question \"{q.Text}\" with Answer \"{q.Answer}\" of Category \"{q.Cat}\" and Subcategory \"{q.SubCat}\" of Difficulty \"{q.Diff}\"");
+                await ch.SendMessageAsync($"{q.ID}: Question \"{q.Text}\" with Answer \"{q.Answer}\" of Category \"{q.Cat}\" and Subcategory \"{q.SubCat}\" of Difficulty \"{q.Diff}\"");
             }
         }
     }
